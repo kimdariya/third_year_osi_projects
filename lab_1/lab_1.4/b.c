fb0a3552f13cc2d1bddb1b16c560fdc215603b08
Kim Dariya Alexandrovna <dkim5354@gmail.com>
2026-09-25 09:51:35 +0700

    Create b.c
    b. Измените программу так чтобы поточная функция ничего не распечатывала, а в бесконечном цикле увеличивала счетчик на 1. Используйте pthread_cancel() для того чтобы ее остановить. Объясните результат. Что можно сделать, чтобы
    pthread_cancel() прервал поток?

---
+++ lab_1/lab_1.4/b.c
@@ -0,0 +1,33 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void *mythread(void *arg) {
+  	int counter = 0;
+  	while(1){
+  		counter++;
+  		pthread_testcancel();
+  	}
+  	return NULL;
+}
+
+int main() {
+  	pthread_t tid;
+  	int err;
+  
+  	printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+  
+  	err = pthread_create(&tid, NULL, mythread, NULL);
+  	if (err) {
+  	    printf("main: pthread_create() failed: %s\n", strerror(err));
+  		return -1;
+  	}
+  	
+  	pthread_cancel(tid);
+  
+  	return 0;
+}
