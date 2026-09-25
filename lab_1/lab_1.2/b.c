1d429c7d7a2d169cf4ede32b700e2df0841e795e
Kim Dariya Alexandrovna <dkim5354@gmail.com>
2026-09-25 09:33:35 +0700

    Create b.c
    b. Измените программу так чтобы созданный поток возвращал число 42, а основной поток получал это число и распечатывал.

---
+++ lab_1/lab_1.2/b.c
@@ -0,0 +1,36 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void *mythread(void *arg) {
+        printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());   
+ 	      return (void*)(long)42;
+}
+
+int main() {
+        pthread_t tid;
+        int err;
+	      void* number;
+
+        printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+
+        err = pthread_create(&tid, NULL, mythread, NULL);
+        if (err) {
+            printf("main: pthread_create() failed: %s\n", strerror(err));
+                return -1;
+        }
+
+        err = pthread_join(tid,&number);
+	      if(err) {
+		        printf("main: pthread_join() failed: %s\n", strerror(err));
+		        return -1;
+	      }
+
+	      printf("result: %ld\n", (long)number);
+
+        return 0;
+}
