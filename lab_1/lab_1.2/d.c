2580f09a29d697b06bde87bbf8c7c42f31891863
Kim Dariya Alexandrovna <dkim5354@gmail.com>
2026-09-25 09:35:23 +0700

    Create d.c
    d. Напишите программу, которая в бесконечном цикле будет создавать поток, с поточной функцией, которая выводит свой идентификатор потока и завершается. Запустите. Объясните результат.

---
+++ lab_1/lab_1.2/d.c
@@ -0,0 +1,36 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void *mythread(void *arg) {
+        printf("mythread [TID = %d]\n", gettid());
+ 	      return NULL;
+}
+
+int main() {
+        pthread_t tid;
+        int err;
+	      int thread_num;
+
+        printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+
+	      while(1){
+        	  err = pthread_create(&tid, NULL, mythread, NULL);
+        	  if (err) {
+            		  printf("main: pthread_create() failed: %s\n", strerror(err));
+                	return -1;
+        	  }
+
+        	  err = pthread_join(tid, NULL);
+		        if(err) {
+			            printf("main: pthread_join() failed: %s\n", strerror(err));
+			            return -1;
+		        }
+	      }
+
+        return 0;
+}
