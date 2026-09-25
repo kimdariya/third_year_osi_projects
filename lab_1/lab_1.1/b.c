eb31355f6cb57e261088aef43ba3d4636048a459
Kim Dariya Alexandrovna <dkim5354@gmail.com>
2026-09-25 09:39:20 +0700

    Update b.c

--- lab_1/lab_1.1/b.c
+++ lab_1/lab_1.1/b.c
@@ -19,10 +19,10 @@ int main() {
 
 	for (int i = 0; i<5; i++){
 		err = pthread_create(&tid[i], NULL, mythread, NULL);
-	        if (err) {
-        	    printf("main: pthread_create() failed: %s\n", strerror(err));
-                    return -1;
-        	}
+	    if (err) {
+        	printf("main: pthread_create() failed: %s\n", strerror(err));
+            return -1;
+        }
 	}
 
 	pthread_exit(NULL);
